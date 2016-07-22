3910bf8dd3c842372be0946a9c0af737aa0dcafc
matthew <matthew@zumigo.com>
2016-07-22 14:51:27 -0700

    refactored routes and controller

--- app/controller.js
+++ app/controller.js
@@ -1,18 +1,16 @@
 var request = require('request')
 
-var onRes = (error, response, body, resolve, reject) => {
-	if (error)
-		reject(error)
-	resolve(body)
+module.exports = {
+	doSomething : doSomething
 }
 
-module.exports = {
+function doSomething(req, res) {
+	request("http://angular.io", (error, response, body) =>
+		onRes(error, response, body, res) )
+}
 
-	doSomething : () => {
-		return new Promise((resolve, reject) => {
-			request("http://angular.io", (error, response, body) =>
-				onRes(error, response, body, resolve, reject))
-		})
-	}
-
+function onRes(error, response, body, res) {
+	if (error)
+		res.end(error)
+	res.end(body)
 }
--- app/routes.js
+++ app/routes.js
@@ -1,19 +1,9 @@
 var controller = require('./controller')
 
-var onSuc = (response, res) =>
-	res.end(response)
-
-var onErr = (error, res) =>
-	res.end(error)
-
 module.exports = (app) => {
 
-	app.get('/doSomething', (req, res) => {
-		controller.doSomething()
-			.then((response) => onSuc(response, res), (error) => onErr(error, res))
-	})
+	app.get('/doSomething', controller.doSomething)
 
 	app.get('*', (req, res) =>
-		res.sendFile(__dirname + '/public/index.html')
-	)
+		res.sendFile(__dirname + '/public/index.html') )
 }
